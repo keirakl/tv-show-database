98c9e57060b6b5b6ceca3e37428c733ff9c55fc0
Keira Luong <ky.keira@gmail.com>
2021-07-30 19:25:44 -0700

    Stripped HTML tags from show summary

--- src/components/ShowInfoModal.js
+++ src/components/ShowInfoModal.js
@@ -4,78 +4,84 @@ import Image from 'react-bootstrap/Image';
 import './style.css';
 
 const ShowInfoModal = (props) => {
-        const show = props.showInfo;
-        return (
-            <Modal show={props.showModal} onHide={() => props.closeModal()} size="lg" >
-                <Modal.Header closeButton>
-                </Modal.Header>
-                <Modal.Body>
-                    {show?
-                        <Container>
-                            <Row>
-                                <h1>{show.name}</h1>
-                            </Row>
-                            <Row>
-                                <Col sm="auto">
-                                    {show.image?
-                                        <Image style={{maxWidth: 400}} src={show.image.original} fluid />
+    const show = props.showInfo;
+    return (
+        <Modal show={props.showModal} onHide={() => props.closeModal()} size="lg" >
+            <Modal.Header closeButton>
+            </Modal.Header>
+            <Modal.Body>
+                {show?
+                    <Container>
+                        <Row>
+                            <h1>{show.name}</h1>
+                        </Row>
+                        <Row>
+                            <Col sm="auto">
+                                {show.image?
+                                    <Image style={{maxWidth: 400}} src={show.image.original} fluid />
+                                    : ""
+                                    }
+                            </Col>
+                            <Col>
+                                <ListGroup variant="flush">
+                                    {show.summary?
+                                        <ListGroup.Item>
+                                            {stripHTMLTags(show.summary)}
+                                        </ListGroup.Item>
                                         : ""
-                                        }
-                                </Col>
-                                <Col>
-                                    <ListGroup variant="flush">
-                                        {show.summary?
-                                            <ListGroup.Item>
-                                                {show.summary}
-                                            </ListGroup.Item>
-                                            : ""
-                                        }
-                                        {show.genres.length > 0?
-                                            <ListGroup.Item>
-                                                <p>Genres: {show.genres.map(genre => `${genre}, `)}...</p>
-                                            </ListGroup.Item>   
-                                            : ""
-                                        }
-                                        {show.language?
-                                            <ListGroup.Item>
-                                                <p>Language: {show.language}</p>
-                                            </ListGroup.Item>  
-                                            : ""
-                                        }
-                                        {show.officialSite?
-                                            <ListGroup.Item>
-                                                <p>Website: <a href={show.officialSite} target="_blank" rel="noreferrer">{show.officialSite}</a></p>
-                                            </ListGroup.Item>  
-                                            : ""
-                                        }
-                                        {show.premiered?
-                                            <ListGroup.Item>
-                                                <p>Premiered on: {show.premiered}</p>
-                                            </ListGroup.Item>  
-                                            : ""
-                                        }
-                                        {show.rating.average?
-                                            <ListGroup.Item>
-                                                <p>Rating: {show.rating.average}</p>
-                                            </ListGroup.Item>   
-                                            : ""
-                                        }
-                                        {show.url?
-                                            <ListGroup.Item>
-                                                <a href={show.url} target="_blank" rel="noreferrer">Read more...</a>
-                                            </ListGroup.Item>   
-                                            : ""
-                                        }
-                                    </ListGroup>
-                                </Col>
-                            </Row>
-                        </Container> 
-                        : <p>No data to show</p>
-                    }
-                    
-                </Modal.Body>
-            </Modal>
-        )
-    }
+                                    }
+                                    {show.genres.length > 0?
+                                        <ListGroup.Item>
+                                            <p>Genres: {show.genres.map(genre => `${genre}, `)}...</p>
+                                        </ListGroup.Item>   
+                                        : ""
+                                    }
+                                    {show.language?
+                                        <ListGroup.Item>
+                                            <p>Language: {show.language}</p>
+                                        </ListGroup.Item>  
+                                        : ""
+                                    }
+                                    {show.officialSite?
+                                        <ListGroup.Item>
+                                            <p>Website: <a href={show.officialSite} target="_blank" rel="noreferrer">{show.officialSite}</a></p>
+                                        </ListGroup.Item>  
+                                        : ""
+                                    }
+                                    {show.premiered?
+                                        <ListGroup.Item>
+                                            <p>Premiered on: {show.premiered}</p>
+                                        </ListGroup.Item>  
+                                        : ""
+                                    }
+                                    {show.rating.average?
+                                        <ListGroup.Item>
+                                            <p>Rating: {show.rating.average}</p>
+                                        </ListGroup.Item>   
+                                        : ""
+                                    }
+                                    {show.url?
+                                        <ListGroup.Item>
+                                            <a href={show.url} target="_blank" rel="noreferrer">Read more...</a>
+                                        </ListGroup.Item>   
+                                        : ""
+                                    }
+                                </ListGroup>
+                            </Col>
+                        </Row>
+                    </Container> 
+                    : <p>No data to show</p>
+                }
+                
+            </Modal.Body>
+        </Modal>
+    )
+}
+
+const stripHTMLTags = (html) => {
+    let doc = new DOMParser().parseFromString(html, 'text/html');
+   return doc.body.textContent || "";
+}
+
 
 export default ShowInfoModal;
